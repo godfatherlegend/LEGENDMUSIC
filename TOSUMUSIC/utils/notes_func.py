3bb3a387cbacdf38cc2d70460ba2b1c9a724f687
godfatherlegend <142713010+godfatherlegend@users.noreply.github.com>
2024-04-18 02:15:33 +0530

    Update notes_func.py

--- TOSUMUSIC/utils/notes_func.py
+++ TOSUMUSIC/utils/notes_func.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 import html
 import re
 from pyrogram.types import InlineKeyboardMarkup
-from GOKUMUSIC import app
-from GOKUMUSIC.mongo.notesdb import GetNote
+from TOSUMUSIC import app
+from TOSUMUSIC.mongo.notesdb import GetNote
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from GOKUMUSIC.utils.msg_types import button_markdown_parser
+from TOSUMUSIC.utils.msg_types import button_markdown_parser
 
 class NoteTypeMap(Enum):
     text = auto()
