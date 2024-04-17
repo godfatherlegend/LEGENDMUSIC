47a963053a818051b673c30eb51449dc3caf2817
godfatherlegend <142713010+godfatherlegend@users.noreply.github.com>
2024-04-18 00:10:03 +0530

    Add files via upload

---
+++ Strings/__init__.py
@@ -0,0 +1,34 @@
+import os
+from typing import List
+
+import yaml
+
+languages = {}
+languages_present = {}
+
+
+def get_string(lang: str):
+    return languages[lang]
+
+
+for filename in os.listdir(r"./Strings/languages/"):
+    if "en" not in languages:
+        languages["en"] = yaml.safe_load(
+            open(r"./Strings/languages/en.yml", encoding="utf8")
+        )
+        languages_present["en"] = languages["en"]["name"]
+    if filename.endswith(".yml"):
+        language_name = filename[:-4]
+        if language_name == "en":
+            continue
+        languages[language_name] = yaml.safe_load(
+            open(r"./Strings/languages/" + filename, encoding="utf8")
+        )
+        for item in languages["en"]:
+            if item not in languages[language_name]:
+                languages[language_name][item] = languages["en"][item]
+    try:
+        languages_present[language_name] = languages[language_name]["name"]
+    except:
+        print("There is some issue with the language file inside bot.")
+        exit()
