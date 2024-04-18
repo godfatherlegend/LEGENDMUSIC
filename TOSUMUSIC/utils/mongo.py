69d03234710bbfaf0f1036e7837424bcb1620ca4
godfatherlegend <142713010+godfatherlegend@users.noreply.github.com>
2024-04-18 07:34:39 +0530

    Update mongo.py

--- TOSUMUSIC/utils/mongo.py
+++ TOSUMUSIC/utils/mongo.py
@@ -6,7 +6,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from config import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
-db = mongo.GOKUMUSIC
+db = mongo.TOSUMUSIC
 
 coupledb = db.couple
 
