ed2cb9e1689ae41d526577bd0d9be36c8c640185
godfatherlegend <142713010+godfatherlegend@users.noreply.github.com>
2024-04-18 18:08:00 +0530

    Update bot.py

--- TOSUMUSIC/core/bot.py
+++ TOSUMUSIC/core/bot.py
@@ -6,11 +6,11 @@ import config
 from ..logging import LOGGER
 
 
-class GOKU(Client):
+class TOSU(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="GOKUMUSIC",
+            name="TOSUMUSIC",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
