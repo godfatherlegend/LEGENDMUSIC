b46882003874ec2f178985639df507f1d7943581
godfatherlegend <142713010+godfatherlegend@users.noreply.github.com>
2024-04-19 11:03:18 +0530

    Update Youtube.py

--- TOSUMUSIC/platforms/Youtube.py
+++ TOSUMUSIC/platforms/Youtube.py
@@ -10,8 +10,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from GOKUMUSIC.utils.database import is_on_off
-from GOKUMUSIC.utils.formatters import time_to_seconds
+from TOSUMUSIC.utils.database import is_on_off
+from TOSUMUSIC.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
